cbe9bcd037fd903989c259caa75d49259cecd64d
Robin Bastian <robin.bastian@globalpay.id>
2018-11-27 00:02:50 +0700

    Update some test assertion technique

--- services/product_test.go
+++ services/product_test.go
@@ -35,6 +35,7 @@ func (repo *productRepoMock) Create(*models.Product) error {
 }
 
 func Test_IProductService_List(t *testing.T) {
+	assert := assert.New(t)
 	mockRepo := productRepoMock{}
 
 	t.Log("1. Expect success with 1 product listed")
@@ -47,29 +48,30 @@ func Test_IProductService_List(t *testing.T) {
 	})
 	serv := services.NewProductService(&mockRepo)
 	prod, err := serv.List(1, 10)
-	assert.Nil(t, err, "No error")
-	assert.Equal(t, 1, len(prod), "List returned is of size: 1")
-	assert.Equal(t, int64(1), prod[0].ID, "Product ID is 1")
+	assert.Nil(err, "No error")
+	assert.Equal(1, len(prod), "List returned is of size: 1")
+	assert.Equal(int64(1), prod[0].ID, "Product ID is 1")
 
 	//---
 	t.Log("2. Expect error: page must be >= 1")
 	prod, err = serv.List(0, 10)
-	assert.Error(t, err, "Page must be greater than zero")
+	assert.Error(err, "Page must be greater than zero")
 
 	//---
 	t.Log("3. Expect error: rows per page must be >= 10")
 	prod, err = serv.List(1, 1)
-	assert.Error(t, err, "RPP must be greater than 10")
+	assert.Error(err, "RPP must be greater than 10")
 
 	//---
 	t.Log("4. Expect error: something wrong with the DB, error code 500")
 	mockRepo.listErr = errors.New("Something wrong with the DB")
 	prod, err = serv.List(1, 10)
-	assert.Error(t, err, "Something wrong with the DB")
-	assert.Equal(t, 500, err.Code)
+	assert.Error(err, "Something wrong with the DB")
+	assert.Equal(500, err.Code)
 }
 
 func Test_IProductService_Find(t *testing.T) {
+	assert := assert.New(t)
 	mockRepo := productRepoMock{}
 
 	t.Log("1. Expect success find product")
@@ -82,32 +84,33 @@ func Test_IProductService_Find(t *testing.T) {
 	}
 	serv := services.NewProductService(&mockRepo)
 	prod, err := serv.Find(1)
-	assert.Nil(t, err, "No error")
-	assert.Equal(t, int64(1), prod.ID, "Product ID is 1")
+	assert.Nil(err, "No error")
+	assert.Equal(int64(1), prod.ID, "Product ID is 1")
 
 	//---
 	t.Log("2. Expect error: id must be >= 1")
 	prod, err = serv.Find(0) //assume db serial starts with 1
-	assert.Error(t, err, "ID parameter must exists")
+	assert.Error(err, "ID parameter must exists")
 
 	//---
 	t.Log("3. Expect error: not found")
 	mockRepo.findErr = orm.ErrNoRows
 	prod, err = serv.Find(1)
 	mockRepo.findErr = nil
-	assert.Error(t, err, "Error product not found")
-	assert.Equal(t, 404, err.Code)
+	assert.Error(err, "Error product not found")
+	assert.Equal(404, err.Code)
 
 	//---
 	t.Log("4. Expect error: something wrong with the DB")
 	mockRepo.findErr = errors.New("Something wrong with the DB")
 	prod, err = serv.Find(1)
 	mockRepo.findErr = nil
-	assert.Error(t, err, "Error product not found")
-	assert.Equal(t, 500, err.Code)
+	assert.Error(err, "Error product not found")
+	assert.Equal(500, err.Code)
 }
 
 func Test_IProductService_Create(t *testing.T) {
+	assert := assert.New(t)
 	mockRepo := productRepoMock{}
 
 	t.Log("1. Expect success create product")
@@ -118,9 +121,9 @@ func Test_IProductService_Create(t *testing.T) {
 		"price": 50000
 	}`
 	prod, err := serv.Create([]byte(jsonReq))
-	assert.Nil(t, err, "No error")
-	assert.Equal(t, "Product 1", prod.Name, "Product name is Product 1")
-	assert.NotEqual(t, 0, prod.CreateTs, "Product created timestamp is filled")
+	assert.Nil(err, "No error")
+	assert.Equal("Product 1", prod.Name, "Product name is Product 1")
+	assert.NotEqual(0, prod.CreateTs, "Product created timestamp is filled")
 
 	//---
 	t.Log("2. Expect error: price not defined")
@@ -129,8 +132,8 @@ func Test_IProductService_Create(t *testing.T) {
 		"category": "CAT-1"
 	}`
 	prod, err = serv.Create([]byte(jsonReq))
-	assert.Error(t, err, "Product price must be > 0")
-	assert.Equal(t, 400, err.Code)
+	assert.Error(err, "Product price must be > 0")
+	assert.Equal(400, err.Code)
 
 	//---
 	t.Log("3. Expect error: category not defined")
@@ -139,8 +142,8 @@ func Test_IProductService_Create(t *testing.T) {
 		"price": 50000
 	}`
 	prod, err = serv.Create([]byte(jsonReq))
-	assert.Error(t, err, "Product category must be filled")
-	assert.Equal(t, 400, err.Code)
+	assert.Error(err, "Product category must be filled")
+	assert.Equal(400, err.Code)
 
 	//---
 	t.Log("4. Expect error: name not defined")
@@ -149,16 +152,16 @@ func Test_IProductService_Create(t *testing.T) {
 		"price": 50000
 	}`
 	prod, err = serv.Create([]byte(jsonReq))
-	assert.Error(t, err, "Product name must be filled")
-	assert.Equal(t, 400, err.Code)
+	assert.Error(err, "Product name must be filled")
+	assert.Equal(400, err.Code)
 
 	//---
 	t.Log("5. Expect error: invalid JSON")
 	jsonReq = `i am not a json`
 	prod, err = serv.Create([]byte(jsonReq))
-	assert.Error(t, err, "Product name must be filled")
-	assert.Equal(t, 400, err.Code)
-	assert.Equal(t, "Invalid JSON request", err.Message)
+	assert.Error(err, "Product name must be filled")
+	assert.Equal(400, err.Code)
+	assert.Equal("Invalid JSON request", err.Message)
 
 	//---
 	t.Log("6. Expect error: Something wrong with the DB")
@@ -169,6 +172,6 @@ func Test_IProductService_Create(t *testing.T) {
 	}`
 	mockRepo.createErr = errors.New("Something wrong with the DB")
 	prod, err = serv.Create([]byte(jsonReq))
-	assert.Error(t, err, "Something wrong with the DB")
-	assert.Equal(t, 500, err.Code)
+	assert.Error(err, "Something wrong with the DB")
+	assert.Equal(500, err.Code)
 }
